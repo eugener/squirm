acc632dd15e5de77ab6a8c1815582b2fe71598b9
Eugene Ryzhikov <eryzhikov@gmail.com>
2012-05-13 15:04:24 -0400

    Core code. Scalatra support

--- squirm/squirm-core/src/main/scala/org/oxbow/squirm/Components.scala
+++ squirm/squirm-core/src/main/scala/org/oxbow/squirm/Components.scala
@@ -1,5 +1,136 @@
 package org.oxbow.squirm
 
-object Components {
+import scala.xml.NodeSeq
 
+abstract class Component {
+    def render: NodeSeq 
+}
+
+protected object Renderer {
+   def renderComponents(components: Iterable[Component]) =
+      components.foldLeft(NodeSeq.Empty)(_ ++ _.render)
+}
+
+import Renderer._
+
+//////// PAGE //////////////////////////////////////////////////////////////////////////////////////////////////
+
+case class Page(title: String = "", headerTitle: String = "")( components: Component* ) extends Component {
+
+   private def template( elements: => NodeSeq ) =
+
+    <html>
+      <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>{ title }</title>
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.css"/>
+        <link rel="stylesheet" href="docs/_assets/css/jqm-docs.css"/>
+        <script src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
+        <script src="experiments/themeswitcher/jquery.mobile.themeswitcher.js"></script>
+        <script src="docs/_assets/js/jqm-docs.js"></script>
+        <script src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
+      </head>
+      <body>
+          <div data-role="page" data-add-back-btn="true" >
+             { PageHeader( headerTitle ).render }
+             <div data-role="content">{elements}</div>
+          </div>
+      </body>
+    </html>
+  
+   override def render: NodeSeq = template( renderComponents(components) ) 
+  
+
+}
+
+case class PageHeader(title: String = "Header", dataTheme: String = "a" ) extends Component {
+
+  /**
+   * This method can be overriden to provide different content
+   */
+  protected val content: NodeSeq = <h1>{ title }</h1>
+  override val render: NodeSeq = <div data-role="header" data-theme={dataTheme}>{ content }</div>
+
+}
+
+//////// LIST VIEW //////////////////////////////////////////////////////////////////////////////////////////////////
+
+case class ListView( insets: Boolean = true, content: NodeSeq  ) extends Component {
+   override val render: NodeSeq = 
+       <ul data-role="listview" data-inset={insets.toString} data-theme="d" >{content}</ul>
+}
+
+object ListView {
+   def apply( components: Component* ): ListView = new ListView( insets = true, renderComponents(components))
+}
+
+case class LinkListItem( title: String, url: String ) extends Component {
+   override val render: NodeSeq = <li><a href={url}>{title}</a></li>
+}
+
+case class PropertyListItem( title: String, value: String ) extends Component {
+   override val render: NodeSeq =
+       <li>{title} <p class="ui-li-aside"><strong>{value}</strong></p></li> 
+}
+
+//////// COLLAPSIBLES ////////////////////////////////////////////////////////////////////////////////////////////////
+
+case class Collapsible( title: String, collapsed: Boolean, components: List[Component] ) extends Component {
+  override val render: NodeSeq = 
+     <div data-role="collapsible" data-collapsed={collapsed.toString} data-theme="b" data-content-theme="d" >
+         <h6>{title}</h6>
+         <p>{ ListView(true, renderComponents(components)).render}</p>
+     </div>
+}
+
+object Collapsible {
+  
+  def apply( title: String, collapsed: Boolean, components: Component*) = 
+      new Collapsible( title, collapsed, components.toList )
+  
+  def apply( title: String, components: Component*) = 
+      new Collapsible( title, true, components.toList )
+  
+}
+
+
+case class CollapsibleSet( elements: Collapsible* ) extends Component {
+   override val render: NodeSeq = 
+      <div data-role="collapsible-set" >{ renderComponents(elements)}</div>
+}
+
+//////// FIELDS ////////////////////////////////////////////////////////////////////////////////////////////////
+
+case class Form( action: String, method: String ="post", message: String = "" )( components: Component* ) extends Component {
+
+   // paramtrize data-ajax, data-transition and theme 
+    
+   override def render: NodeSeq = 
+       <form action={action} method={method} data-ajax="false" data-transition="pop">
+	      <div class="ui-body ui-body-d">
+          {
+              var content = if ( !message.trim.isEmpty ) { 
+                 Seq(PageHeader( message, "e" )) ++ components  
+              } else components
+              renderComponents( content ) 
+          }
+          </div>
+       </form>
+
+}
+
+
+case class Field(title: String, id: String, fieldType: String = "text", required: Boolean = true, defaultValue: String = "" ) extends Component {
+    override val render: NodeSeq =
+        <div data-role="fieldcontain">
+            <label for={ id }>{ title }</label>
+            <input type={ fieldType } name={ id } id={ id } value={ defaultValue } class={ if (required) "required" else "" }/>
+        </div>
+
+}
+
+case class SubmitButton( title: String = "Submit" ) extends Component {
+    override val render: NodeSeq = 
+        <button type="submit" data-theme="b" name="submit" value="submit-value">{title}</button> 
 }
